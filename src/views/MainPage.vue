34c4210a6c26fab3c6103c83b04a175b710cb9b4
subeen <sju081902@gmail.com>
2025-04-08 16:13:36 +0900

    feat-dashboard : RecentTransactionList.vue 파일 추가

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,10 +1,37 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, computed } from 'vue';
+import axios from 'axios';
 import MonthlyChart from '@/components/charts/MonthlyChart.vue';
+import DoughnutChart from '@/components/charts/DoughnutChart.vue';
+import RecentTransactionList from '@/components/transactions/RecentTransactionList.vue';
+import AddButton from '@/components/buttons/AddButton.vue';
+import { useUserStore } from '@/store/userStore';
+
+const userStore = useUserStore();
+const userId = userStore.user?.id;
+
+const trades = ref([]);
+
+// 데이터 불러오기
+onMounted(async () => {
+  const api = axios.create({ baseURL: 'http://localhost:3000' });
+  const res = await api.get(`/users/${userId}/trade_list`);
+  trades.value = res.data;
+});
+
+// 최근 5개 거래(지출) 데이터 추출
+const recentTransactions = computed(() => {
+  return trades.value
+    .sort((a, b) => new Date(b.date) - new Date(a.date))
+    .slice(0, 5);
+});
 </script>
 
 <template>
-  <MonthlyChart :data="statsByCategory" />
+  <MonthlyChart />
+  <DoughnutChart />
+  <RecentTransactionList :transactions="recentTransactions" />
+  <AddButton />
 </template>
 
 <style scoped></style>
