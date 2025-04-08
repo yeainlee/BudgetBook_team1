8bc58d0db30602d459e6e992e9607901776bfe2f
ra-ran <ucreo@naver.com>
2025-04-08 16:23:01 +0900

    fix-App : App.vue 수정

--- src/main.js
+++ src/main.js
@@ -11,10 +11,5 @@ const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia); // 먼저 pinia를 앱에 등록
 
-// 이제 pinia가 활성화된 후에 스토어를 사용
-import { useUserStore } from './store/index';
-const userStore = useUserStore();
-userStore.initializeStore();
-
 app.use(router);
 app.mount('#app');
