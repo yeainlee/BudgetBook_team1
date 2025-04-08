eb68a5f0353f766241a065b3677b5eb04deb30ad
권혁주\rnjsg <rnjsgurwn990217@naver.com>
2025-04-08 17:12:15 +0900

    transactions-form : 수정

---
+++ src/views/TransactionsEditPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
+
+<script setup></script>
+
+<template>
+  <!-- <RouterView /> -->
+  <!-- <ToastNotification /> -->
+</template>
+
+<style scoped></style>
